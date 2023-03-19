0d0b30fd274ca75afff3d4701878ccdf5348ce96
macnd <johnny187896@live.co.uk>
2023-03-19 01:36:19 +0000

    Remove suspense

--- src/views/KitView.vue
+++ src/views/KitView.vue
@@ -8,10 +8,8 @@ let route = useRoute();
 </script>
 
 <template>
-  <Suspense>
+
     <KitDetails :key="$route.params.id" v-if="route.params.id"/>
     <Kits v-else />
-  </Suspense>
 
-  <!-- <v-img src="https://pbs.twimg.com/media/Fqf9RUkXsAUkKdD?format=jpg&name=large"></v-img> -->
 </template>
